b9a9a4a7439aba91cce24c78f4f217a7d1f8fb4b
aSumo <1xtelescope@gmail.com>
2026-07-17 21:49:29 +0900

    feat: Make a tutorial bot

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from iscat!")
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 
-if __name__ == "__main__":
-    main()
+# Intentsを設定
+intents = discord.Intents.default()
+intents.message_content = True  # on_messageを使うなら必須
+
+# Client作成時にintentsを渡す
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"ログインしました: {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    if message.content == "!neko":
+        await message.channel.send("にゃーん")
+
+
+client.run(TOKEN)
